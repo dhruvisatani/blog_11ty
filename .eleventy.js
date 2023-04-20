e10808e05cf6679765c31d2d320e67c033da94bc
Dhruvi satani <dhruvi.wbot@gmail.com>
2023-04-20 17:14:00 +0530

    11ty file

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,19 @@
-module.exports = function (eleventyConfig) {
-  
-    eleventyConfig.addPassthroughCopy('./src/style.css');
-    eleventyConfig.addPassthroughCopy('./src/assets');
+const { DateTime } = ("luxon");
 
-  
-    return{
-    dir :{
-        input:"src",
-        oninput: "public"
+
+module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy('./src/style.css');
+  eleventyConfig.addPassthroughCopy('./src/assets');
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
+  return {
+    dir: {
+      input: "src",
+      oninput: "public"
     }
   };
 }
